f3e9123d3de29ff77f9a8fd15437899918c341b7
Gustavo Ottoboni <ottoguz87@gmail.com>
2023-07-13 12:54:12 -0300

    Update: add validations to auth-credentials.dto.ts

--- src/auth/auth-credentials.dto.ts
+++ src/auth/auth-credentials.dto.ts
@@ -1,10 +1,26 @@
 /* eslint-disable prettier/prettier */
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 import { AccountTypeEnum } from './account-type.enum';
 
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
   firstName: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
   lastName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  //Regex para verificar uso de caracteres especiais Upper e Lower e numero
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'Senha muito fraca!'})
   password: string;
   accountType: AccountTypeEnum;
 }
