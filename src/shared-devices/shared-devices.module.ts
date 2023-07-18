57f92fb0b9472f10616c33e1d7ba7474884a4b9d
Gustavo Ottoboni <ottoguz87@gmail.com>
2023-07-17 22:45:17 -0300

    Update: Add UsersRep and DevRep to shr-devs module

--- src/shared-devices/shared-devices.module.ts
+++ src/shared-devices/shared-devices.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SharedDevicesController } from './shared-devices.controller';
 import { SharedDevicesService } from './shared-devices.service';
+import { UsersRepository } from 'src/auth/users.repository';
+import { DevicesRepository } from 'src/device/devices.repository';
 
 @Module({
   controllers: [SharedDevicesController],
-  providers: [SharedDevicesService]
+  providers: [SharedDevicesService, UsersRepository, DevicesRepository],
 })
 export class SharedDevicesModule {}
