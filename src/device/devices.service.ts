dc24130556d93af5413f28678a55576b1256eff2
Gustavo Ottoboni <ottoguz87@gmail.com>
2023-07-20 22:47:30 -0300

    Changes: Val will be encaps in sharedDevs

--- src/device/devices.service.ts
+++ src/device/devices.service.ts
@@ -51,39 +51,18 @@ export class DevicesService {
     name: string,
   ): Promise<Device> {
     const device = await this.getDeviceById(deviceId);
-    const sharingLevel = this.sharedDevicesRepository.findOneBy({ deviceId })
-    if ((await sharingLevel).sharingLevel === 'OWNER' || 
-        (await sharingLevel).sharingLevel === 'EDITOR' && 
-         device.deviceId === (await sharingLevel).deviceId) {
-      device.type = type;
-      device.local = local;
-      device.name = name;
-      await this.devicesRepository.save(device);
-      return device;
-    } else {
-      throw new UnauthorizedException(
-        'This device can only be edited by owners or editors!',
-      );
-    }
+    device.type = type;
+    device.local = local;
+    device.name = name;
+    await this.devicesRepository.save(device);
+    return device;
   }
 
-   // Faz uma busca de um dispositivo pelo "id" no repositório e deleta
-  async deleteDevice(deviceId: string, user: User): Promise<void> {
-    const device = await this.getDeviceById(deviceId);
-    const sharedDevice = await this.sharedDevicesRepository.findOneBy({ deviceId })
-    console.log(sharedDevice.sharingLevel === 'OWNER' && sharedDevice.deviceId === deviceId ||
-    sharedDevice.userId === device.user.userId)
-    
-    if (sharedDevice.sharingLevel === 'OWNER' && sharedDevice.deviceId === device.deviceId ||
-        sharedDevice.sharingLevel === 'OWNER' && sharedDevice.userId === user.userId) {
-      const result = await this.devicesRepository.delete(sharedDevice.deviceId);
-      if (result.affected === 0) {
-        throw new NotFoundException(`Device with ID: ${deviceId} not found!`);
-      } 
-    } else {
-      throw new UnauthorizedException(
-        'This device can only be deleted by its owner!',
-      );
-    }
+  // Faz uma busca de um dispositivo pelo "id" no repositório e deleta
+  async deleteDevice(deviceId: string, _user: User): Promise<void> {
+    const result = await this.devicesRepository.delete(deviceId);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Device with ID: ${deviceId} not found!`);
+    } 
   }
 }
